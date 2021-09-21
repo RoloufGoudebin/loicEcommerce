3be78c47cc03c85c51be53f380817d5b923f2aad
Robin Goudelouf <goudeloufrobin@gmail.com>
2021-09-21 11:06:13 +0200

    git init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
+
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    MDBBootstrapModulesPro.forRoot(),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    MDBSpinningPreloader,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
