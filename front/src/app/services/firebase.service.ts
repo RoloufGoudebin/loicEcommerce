1bc362434b919f2a0e9a12c73c22e04abc17b500
Robin Goudelouf <goudeloufrobin@gmail.com>
2021-09-25 16:19:35 +0200

    add collection

--- front/src/app/services/firebase.service.ts
+++ front/src/app/services/firebase.service.ts
@@ -10,7 +10,12 @@ export class FirebaseService {
   constructor(private firestore: AngularFirestore) {
   }
 
-  createItem(data: Item) {
+
+  /*
+  CRUD pour la collection "items" 
+  */
+
+  createItem(data: any) {
     return new Promise<any>((resolve, reject) => {
       this.firestore
         .collection("items")
@@ -23,18 +28,53 @@ export class FirebaseService {
     return this.firestore.collection("items").snapshotChanges();
   }
 
-  updateItem(data: Item) {
+  updateItem(data: any) {
     return this.firestore
       .collection("items")
       .doc(data.id)
       .set(data);
   }
 
-  deleteItem(data: Item) {
+  deleteItem(data: any) {
     return this.firestore
        .collection("items")
        .doc(data.id)
        .delete();
   }
+
+  /*=====================================================================*/
+
+  /*
+  CRUD pour la collection "collections"
+  */
+
+  createCollection(data:any) {
+    return new Promise<any>((resolve, reject) => {
+      this.firestore
+        .collection("collections")
+        .add(data)
+        .then(res => { }, err => reject(err));
+    });
+  }
+
+  getCollections() {
+    return this.firestore.collection("collections").snapshotChanges();
+  }
+
+  updateCollections(data: any) {
+    return this.firestore
+      .collection("collections")
+      .doc(data.id)
+      .set(data);
+  }
+
+  deleteCollections(data: any) {
+    return this.firestore
+       .collection("collections")
+       .doc(data.id)
+       .delete();
+  }
+
+  /*=====================================================================*/
 
 }
