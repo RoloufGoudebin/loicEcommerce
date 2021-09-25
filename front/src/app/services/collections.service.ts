c0b5ca0bbff7078687582f115fa1dcf41dd7ce5d
Robin Goudelouf <goudeloufrobin@gmail.com>
2021-09-25 16:22:27 +0200

    collection form

--- front/src/app/services/collections.service.ts
+++ front/src/app/services/collections.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,10 @@ import { Injectable } from '@angular/core';
 export class CollectionsService {
 
   constructor() { }
+
+  form = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+  })
+
 }
